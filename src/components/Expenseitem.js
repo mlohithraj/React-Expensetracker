0f4c408a9028965db5b1892015e6a7bdb8032e09
mlohithraj <rlohith767@gmail.com>
2023-12-23 20:20:05 +0530

    Expense

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+        <h2>Expense Item</h2>
+        <h3>Food Rs: 10</h3>
+        <h3>Petro Rs: 100</h3>
+        <h3>Movie Rs: 200</h3>
+        </div>
+    )
+}
+
+export default ExpenseItem;
