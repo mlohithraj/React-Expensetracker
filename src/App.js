cbc4e7a680702d0118f6b9273920e13d35156c47
mlohithraj <rlohith767@gmail.com>
2023-12-25 09:05:08 +0530

    Adding props and looping the expenses

--- src/App.js
+++ src/App.js
@@ -4,10 +4,52 @@ import ExpenseItem from "./components/Expenseitem";
 
 
 function App() {
+
+  const expenses = [
+    {
+      title: 'Car parking',
+      amount: 'Rs: 100',
+      location: 'Theatre',
+      date: new Date(2023, 12, 24)
+    },
+    {
+      title: 'Movie',
+      amount: 'Rs: 500',
+      location: 'Salar',
+      date: new Date(2023, 12, 24)
+    },
+    {
+      title: 'Petrol',
+      amount: 'Rs: 200',
+      location: 'For Movie',
+      date: new Date(2023, 12, 24)
+    },
+    {
+      title: 'Friends',
+      amount: 'Rs: 1000',
+      location: 'Picnic',
+      date: new Date(2023, 12, 25)
+    }
+  ]
+
+  const expenseItems = [];
+
+  for (let i = 0; i < expenses.length; i++) {
+    expenseItems.push(
+      <ExpenseItem
+        key={i} 
+        title={expenses[i].title}
+        amount={expenses[i].amount}
+        location={expenses[i].location}
+        date={expenses[i].date}
+      />
+    );
+  }
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenseItems}
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,10 +1,26 @@
-function ExpenseItem() {
+import './Expenseitem.css';
+
+function ExpenseItem(props) {
+    const expenseDate = new Date(2023, 12, 24);
+    const expenseTitle = 'Parking';
+    const expenseAmount = 'Rs: 25';
+    const expenseLocation = 'Beach';
+
     return (
-        <div>
+        <div className='expense-item'>
         <h2>Expense Item</h2>
-        <h3>Food Rs: 10</h3>
-        <h3>Petro Rs: 100</h3>
-        <h3>Movie Rs: 200</h3>
+        <h2>{props.date.toISOString()}</h2>
+        <div className='expense-item-description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item-price'>
+            <h2>{props.amount}</h2>
+        </div>
+        <div className='expense-item-location'>
+            <h2>{props.location}</h2>
+        </div>
+        {/* <h3>Petro Rs: 100</h3>
+        <h3>Movie Rs: 200</h3> */}
+        </div>
         </div>
     )
 }
