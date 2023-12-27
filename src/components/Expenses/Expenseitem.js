f78b898ebdd900b786d8da8fa82593f8d2a0cd94
mlohithraj <rlohith767@gmail.com>
2023-12-27 19:45:21 +0530

    Added Delete Button

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -2,18 +2,30 @@ import ExpenseTitleAmount from './ExpenseTitleAmount';
 import Card from '../UI/card';
 import ExpenseDate from './ExpenseDate';
 import './Expenseitem.css';
+import { click } from '@testing-library/user-event/dist/click';
 
 const ExpenseItem = (props) => {
+    const clickHandler = () => {
+        console.log('clicked');
+    }
+
+    const deleteButton = () => {
+       const expenseItems1 = document.getElementsByClassName('.expense-item-price')
+       expenseItems1.onClick = expenseItems1.remove();
+    //    expenseItems1.remove();
+    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
         <div className='expense-item-description'>
             <ExpenseTitleAmount title={props.title}/>
+        </div>
         <div className='expense-item-price'>
             <ExpenseTitleAmount amount={props.amount}/>
         </div>
-        </div>
+        <button onClick={clickHandler}>Change Title</button>
+        <button onClick={deleteButton}>Delete Expense</button>
         </Card>
     )
 }
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import App from './components/Expenses/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
